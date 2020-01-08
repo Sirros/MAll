eac42567f6dbe4fccacdef809a1e879b248d16d0
Jacob <121970263@qq.com>
2020-01-08 18:34:56 +0800

    导航栏目录划分

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style scoped>
+</style>
