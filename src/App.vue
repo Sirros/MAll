64cbf1d975f7245052ac13324583bdd4545dd51a
Jacob <121970263@qq.com>
2020-02-02 22:39:30 +0800

    完成TabBar封装

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,17 @@
 <template>
   <div>
-    <tab-bar>
-      <!-- 这里每个item的path，就是传路径字符串给组件 -->
-      <tab-bar-item path='/home'> 
-        <img slot='item-icon' src="./assets/images/tabbar/home.svg" />
-        <img slot='item-active-icon' src="./assets/images/tabbar/home_active.svg" />
-        <div slot='item-title'>首页</div>
-      </tab-bar-item>
-      <tab-bar-item path='/category'>
-        <img slot='item-icon' src="./assets/images/tabbar/category.svg" />
-        <img slot='item-active-icon' src="./assets/images/tabbar/category_active.svg" />
-        <div slot='item-title'>分类</div>
-      </tab-bar-item>
-      <tab-bar-item path='/cart'>
-        <img slot='item-icon' src="./assets/images/tabbar/cart.svg" />
-        <img slot='item-active-icon' src="./assets/images/tabbar/cart_active.svg" />
-        <div slot='item-title'>购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path='/profile'>
-        <img slot='item-icon' src="./assets/images/tabbar/profile.svg" />
-        <img slot='item-active-icon' src="./assets/images/tabbar/profile_active.svg" />
-        <div slot='item-title'>我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TabBar from './components/common/tabbar/TabBar'
-import TabBarItem from './components/common/tabbar/TabBarItem'
+import MainTabBar from './components/content/mainTabbar/MainTabBar'
+
 export default {
   name: 'app',
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 }
 </script>
--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class=""></div>
+  <div class="">
+    <tab-bar>
+      <!-- 这里每个item的path，就是传路径字符串给组件 -->
+      <tab-bar-item path='/home'> 
+        <img slot='item-icon' src="../../../assets/images/tabbar/home.svg" />
+        <img slot='item-active-icon' src="../../../assets/images/tabbar/home_active.svg" />
+        <div slot='item-title'>首页</div>
+      </tab-bar-item>
+      <tab-bar-item path='/category'>
+        <img slot='item-icon' src="../../../assets/images/tabbar/category.svg" />
+        <img slot='item-active-icon' src="../../../assets/images/tabbar/category_active.svg" />
+        <div slot='item-title'>分类</div>
+      </tab-bar-item>
+      <tab-bar-item path='/cart'>
+        <img slot='item-icon' src="../../../assets/images/tabbar/cart.svg" />
+        <img slot='item-active-icon' src="../../../assets/images/tabbar/cart_active.svg" />
+        <div slot='item-title'>购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path='/profile'>
+        <img slot='item-icon' src="../../../assets/images/tabbar/profile.svg" />
+        <img slot='item-active-icon' src="../../../assets/images/tabbar/profile_active.svg" />
+        <div slot='item-title'>我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
 </template>
 
 <script>
+import TabBar from '../../common/tabbar/TabBar'
+import TabBarItem from '../../common/tabbar/TabBarItem'
+
 export default {
-  components:{},
+  components:{
+    TabBar,
+    TabBarItem
+  },
   props:{},
   data(){
     return {
