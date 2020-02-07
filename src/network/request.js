7631fa492a427a944ead584c85a32a6aca0576b4
Jacob <121970263@qq.com>
2020-02-07 23:31:29 +0800

    利用 axios 完成网络封装

--- src/network/request.js
+++ src/network/request.js
@@ -1,14 +1,34 @@
 import axios from 'axios'
 
 export function request(config) {
+  // 1.创建实例
   const instance = axios.create({
     baseURL: 'http://httpbin.org/user-agent',
     timeout: 5000
   })
 
-  // 真正的网络请求
+  // 2.拦截器
+  // 2.1请求拦截器
+  instance.interceptors.request.use(config =>{
+    console.log(config)
+    // 拦截成功要返回数据
+    return config
+  }, err=>{
+    console.log("请求拦截失败")
+  })
+
+  // 2.2响应拦截器
+  instance.interceptors.response.use(res=>{
+    console.log(res)
+    // 拦截成功要返回数据
+    return res
+  }, err=>{
+    console.log("响应拦截失败")
+  })
+
+  // 3.真正的网络请求
   // axios 会返回一个 Promise 
-  return instance
+  return instance(config)
 }
 
 // 调用方法
