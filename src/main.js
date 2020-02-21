b4246c311e126b5becc30666089c7a3178bdc5fd
Jacob <121970263@qq.com>
2020-02-21 17:22:53 +0800

    解决移动端300ms问题

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store/index'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import toast from 'components/common/toast'
+import fastClick from 'fastclick'
 
 // require styles
 // import 'swiper/dist/css/swiper.css'
@@ -13,6 +14,9 @@ Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 // 使用自己创建的插件toast，使用之后会在 toast/index.js 里面进行install方法
 Vue.use(toast)
+
+// 解决移动端300ms问题
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 // new实例作为事件总线
