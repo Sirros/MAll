8abc613647fbd039c8574f22236aa92abfd52166
Jocab <121970263@qq.com>
2020-08-14 11:22:23 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建实例
   const instance = axios.create({
-    baseURL: 'http://106.54.54.237:8000/api/wh',
+    baseURL: 'xxxx',
     timeout: 7000
   })
 
